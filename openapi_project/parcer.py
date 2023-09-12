94b4747f02b6d219d08f6f49649caf7c7936f97a
Natasha-rare <natasha_ea5@mail.ru>
2023-09-12 15:49:03 +0300

    parcer and started settings of django project added. Need to change db in settings and add cinemas data there

---
+++ openapi_project/parcer.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import sqlite3
+
+data = pd.read_csv('../cinemas_data.csv')
+# print(data.info())
+
+conn = sqlite3.connect('cinemas_data.db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS cinemas (
+                    id INTEGER PRIMARY KEY,
+                    cinema_name TEXT,
+                    address TEXT,
+                    description TEXT,
+                    legal_entity TEXT,
+                    website TEXT,
+                    category TEXT,
+                    inn INTEGER)''')
+
+
+for _, row in data.iterrows():
+    name, address, description, ent,\
+        website, category,  inn = row['Название'], row['Адрес'], row['Примечание'], \
+        row['Юридическое лицо'], row['Адрес сайта'], row['Категория'], row['ИНН']
+    # print(name, address, description, ent, website, category,  inn)
+    cursor.execute('''INSERT INTO cinemas (cinema_name, address, description, legal_entity, website, category, inn )
+                              VALUES (?, ?, ?, ? , ?, ?, ?)''', (name, address, description, ent, website, category, inn))
+
+conn.commit()
+conn.close()
