8f8a7d26afb57ee6b8510b9afdd831d578e8785b
Natasha-rare <natasha_ea5@mail.ru>
2023-09-13 01:20:55 +0300

    docker files are added

--- openapi_project/admin.py
+++ openapi_project/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Cinemas
+import os
 # Register your models here.
+# print('i am here', os.listdir('/'), os.listdir())
 # admin.site.register(Cinemas)
 @admin.register(Cinemas)
 class CinamesAdmin(admin.ModelAdmin):
